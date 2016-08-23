0db22ef495287c76df117f1fcefd3402af21cecf
Egbert Bouman <ebouman@gmail.com>
2016-08-23 09:48:37 +0200

    Update 2016-08-23T07:48:35.686Z

--- widget.js
+++ widget.js
@@ -1,14 +1,1 @@
-window.onmessage = function(event) {
-  if (event.data === "close") {
-    $('#widget').remove();
-  }
-};
-
-$(document).ready(function() {
-    // We need to specify the full URL of the iframe. We could work around this with document.currentScript, but there's not enough browser support yet..
-    $(".course-content").append('<iframe id="widget" src="http://localhost:8001/widget.html" scrolling="no" marginheight="0" marginwidth="0" style="position: fixed; bottom: 10px; right: 10px; z-index: 999; height: 100px; width: 370px; border: none;"></iframe>');
-
-    var course = $$course_id;
-    var user = $('.user-link > div').text().trim();
-    console.log('course=' + course + '; user=' + user);
-});
+window.onmessage=function(e){"close"===e.data&&$("#widget").remove()},$(document).ready(function(){$(".course-content").append('<iframe id="widget" src="http://localhost:8001/widget.html" scrolling="no" marginheight="0" marginwidth="0" style="position: fixed; bottom: 10px; right: 10px; z-index: 999; height: 100px; width: 370px; border: none;"></iframe>');var e=$$course_id,i=$(".user-link > div").text().trim();console.log("course="+e+"; user="+i)});
